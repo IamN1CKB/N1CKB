feee92bb78a99a1179a1be81ebf4e179fc2579f7
Nicola Belardo <nicolabelardo005@gmail.com>
2026-09-19 00:28:31 +0200

    Correzione bug

--- Java/algoritmi_e_strutture_dati/Prove_d_esame/provaSettembre2025.java
+++ Java/algoritmi_e_strutture_dati/Prove_d_esame/provaSettembre2025.java
@@ -1,6 +1,6 @@
 package algoritmi_e_strutture_dati.Prove_d_esame;
 
-public class provaSettembre2025 {
+public class ProvaSettembre2025 {
     public static void main(String[] args) {
     // da fare
     }
